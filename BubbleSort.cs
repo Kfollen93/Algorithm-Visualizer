f29a28467f4dc357ee45457d9a1ff676cdc59895
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2021-06-06 17:35:06 -0700

    Create initial bubblesort class

---
+++ BubbleSort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Visualizer
+{
+    class BubbleSort : ISortable
+    {
+        public void RunBubbleSort(int[] arrayToSort, Graphics graphics, int maxVal)
+        {
+            
+        }
+    }
+}
