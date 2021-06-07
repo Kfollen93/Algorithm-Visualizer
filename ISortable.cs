7e792f8f67c65018fc9a06deaecb8f4218496388
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2021-06-06 17:34:52 -0700

    Create interface for sorting

---
+++ ISortable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Visualizer
+{
+    interface ISortable
+    {
+        void RunBubbleSort(int[] arrayToSort, Graphics graphics, int maxVal);
+    }
+}
